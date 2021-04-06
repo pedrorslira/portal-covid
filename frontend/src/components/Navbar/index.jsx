0e27fe055076e83bbb4483218c98aaa9ebc6b93e
Pedro <pedroribeiro64@outlook.com>
2021-04-06 08:38:57 -0300

    Sistema de rotas implementado

---
+++ frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import { Link } from 'react-router-dom';
+
+export default function Header({ title = 'Vacina Covid', routes = [] }) {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand href="/">{title}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          {routes
+            .filter(({ visible = true }) => visible)
+            .map(({ name, path }) => (
+              <Nav.Link as={Link} key={path} to={path}>
+                {name}
+              </Nav.Link>
+            ))}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
---
+++ frontend/src/routes.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Agendamento from './pages/Agendamento';
+import Consulta from './pages/Consulta';
+
+
+const routes = [
+  {
+    component: Home,
+    name: 'Home',
+    path: '/',
+    visible:  false,
+  },
+  {
+    component: Agendamento,
+    name: 'Agendamento',
+    path: '/agendamento',
+  },
+  {
+    component: Consulta,
+    name: 'Consulta',
+    path: '/consulta',
+  },
+  
+];
+
+const Routes = () => (
+  <BrowserRouter>
+    <Navbar title="Vacina Covid" routes={routes} />
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route exact key={path} path={path} component={component} />
+      ))}
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
